77d43f7a46abd321bf6f98cb4b51d306a2634d2f
melvincramer911 <davidbr@davidbr.com>
2020-06-15 14:16:58 -0400

    adding hosts 1-9

--- site.pp
+++ site.pp
@@ -1,4 +1,4 @@
 # site to node mappings !!!
-node 'debrendlinger5c.mylabserver.com','debrendlinger4c.mylabserver.com','debrendlinger3c.mylabserver.com','debrendlinger2c.mylabserver.com' {
+node 'debrendlinger9c.mylabserver.com','debrendlinger8c.mylabserver.com','debrendlinger7c.mylabserver.com','debrendlinger6c.mylabserver.com','debrendlinger5c.mylabserver.com','debrendlinger4c.mylabserver.com','debrendlinger3c.mylabserver.com','debrendlinger2c.mylabserver.com' {
   class { 'nginx': }
 }
